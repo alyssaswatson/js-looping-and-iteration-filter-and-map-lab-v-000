1bd8cd62bbaf4d34f50959804c9203af4933ee82
Alyssa Watson <alyssa.s.watson@gmail.com>
2018-07-07 03:39:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,6 +12,22 @@ function driverNamesWithRevenueOver (drivers, revenue) {
     });
 }
 
-function exactMatch() {
-  
+
+function exactMatch (drivers, matcher) {
+  return drivers.filter(function (driver) {
+    let matches = false;
+
+    for (const key in matcher) {
+      matches = driver[key] === matcher[key];
+    }
+
+    return matches;
+  });
+}
+
+function exactMatchToList (drivers, matcher) {
+  return exactMatch(drivers, matcher)
+    .map(function (driver) {
+      return driver.name;
+    });
 }
